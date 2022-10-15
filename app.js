02ec779d773262896e00b4cf50f976e46d33ac6c
Enkhee <enheeenhee253@gmail.com>
2022-10-15 12:09:18 +0800

    Roll btn darahad shoonii too random-r soligdoj haragddag bolgoloo

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэх утгыг санамсаргүйгээр үүсгэнэ.
-var dice = Math.floor(Math.random() * 6) + 1;
 // ---------------------------<div class="player-score" id="score-0">43</div>
-// document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").innerHTML = "<h4>dice</h4>";
 
 // Програм эхлэхэд бэлтгэж тоонуудыг "0" болгож байна
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 //----------------------------<img src="dice-5.png" alt="Dice" class="dice" />.
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
